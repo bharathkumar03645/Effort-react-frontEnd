7f39782925cb53306d025536f1fbb6770be55e3a
S <2397722@cognizant.com>
2025-06-18 15:27:22 +0530

    changes made

--- src/layout/HRLayout.jsx
+++ src/layout/HRLayout.jsx
@@ -10,46 +10,46 @@ const HRLayout = () => {
 
   return (
     <div className="d-flex vh-100">
-      {/* Sidebar */}
-      <div className="bg-dark text-white p-3" style={{ width: '240px' }}>
-        <h5 className="text-center mb-4">HR PANEL</h5>
-        <ul className="nav flex-column">
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/dashboard" className="nav-link text-white">Dashboard</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/add-trainer" className="nav-link text-white">Add New Trainer</NavLink>
-          </li>
+      
+        <div className="bg-dark text-white p-3" style={{ width: '240px', flexShrink: 0 }}>
+          <h5 className="text-center mb-4">HR PANEL</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/dashboard" className="nav-link text-white">Dashboard</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/add-trainer" className="nav-link text-white">Add New Trainer</NavLink>
+            </li>
 
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/add-cohort" className="nav-link text-white">Add New Cohort</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/allocate-trainer" className="nav-link text-white">Allocate Trainer</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-             <NavLink to="/hr/approvals"  className="nav-link text-white  py-2 px-3 rounded">Approvals</NavLink>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/add-cohort" className="nav-link text-white">Add New Cohort</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/allocate-trainer" className="nav-link text-white">Allocate Trainer</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+           <NavLink to="/hr/approvals"  className="nav-link text-white  py-2 px-3 rounded">Approvals</NavLink>
 
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/search" className="nav-link text-white">Search</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/delete-trainer" className="nav-link text-white">Delete Trainer</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/delete-cohort" className="nav-link text-white">Delete Cohort</NavLink>
-          </li>
-          <li className="nav-item mb-2">
-            <NavLink to="/hr/download" className="nav-link text-white">Download Data</NavLink>
-          </li>
-          <li className="nav-item mt-3">
-            <button className="btn btn-danger w-100" onClick={handleLogout}>Logout</button>
-          </li>
-        </ul>
-      </div>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/search" className="nav-link text-white">Search</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/delete-trainer" className="nav-link text-white">Delete Trainer</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/delete-cohort" className="nav-link text-white">Delete Cohort</NavLink>
+            </li>
+            <li className="nav-item mb-2">
+          <NavLink to="/hr/download" className="nav-link text-white">Download Data</NavLink>
+            </li>
+            <li className="nav-item mt-3">
+          <button className="btn btn-danger w-100" onClick={handleLogout}>Logout</button>
+            </li>
+          </ul>
+        </div>
 
-      {/* Main Content */}
+        {/* Main Content */}
       <div className="flex-grow-1 p-4 bg-light">
         <Outlet />
       </div>
